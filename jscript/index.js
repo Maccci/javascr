a77337b341031884bae88ff66a09c9aa145b70f3
Maccci <118257009+Maccci@users.noreply.github.com>
2023-02-15 18:07:10 +0100

    Add files via upload

---
+++ jscript/index.js
@@ -0,0 +1,43 @@
+
+
+// var namee = "Mario";
+// var age = 22
+// var job = "no job";
+// var gender = "male";
+// var skopje = true;
+
+// var person {
+//     pName = namee;
+//     pAge = age;
+//     pJob = job;
+//     pGender = gender;
+//     pSkopje = skopje;
+    
+// }
+
+
+
+//console.log(person);
+
+// var obj = {name: "John", age: 28, gender: "Male"};
+// console.log(obj);
+
+var namee = “Mario”;
+var age = 22
+var job = “no job”;
+var gender = “male”;
+var skopje = true;
+
+var person {
+pName = namee;
+pAge = age;
+pJob = job;
+pGender = gender;
+pSkopje = skopje;
+}
+
+console.log(person);
+
+<h3>Alert btn</h3>
+
+input type = "button" value = "Click here for alert";
