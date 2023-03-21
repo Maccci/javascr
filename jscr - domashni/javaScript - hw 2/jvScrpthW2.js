5c96b2838d065e3bd4e201fe380cafda3cf5622d
Maccci <118257009+Maccci@users.noreply.github.com>
2023-03-21 11:12:24 +0100

    Add files via upload

---
+++ jscr - domashni/javaScript - hw 2/jvScrpthW2.js
@@ -0,0 +1,28 @@
+let myAge = 19.19.1997;
+let name = 14;
+let yearsSb = 19;
+let yearsUp = 26;
+// let num = Number(myAge);
+// let hghAge = ;  
+
+/*function hghAge(){
+    if(num > name){
+        return "true";          // Create 5th boolean variable that should be true if the first variable is higher then the second
+    }else {                     // variable. Ova e zadachata, ama ne se snaogjam da ja resham.
+        return "false";
+    }
+}
+sum1();       */
+
+function isGreaterThan(value1, value2) {
+    return value1 > value2;
+  }
+  
+  console.log(isGreaterThan(19.19.1997, 14));
+
+// -------------------------------------------------------------------------------------------------------------------------
+
+function dvd(var3, var4){
+    return var3 / var4;
+}
+alert(dvd(26, 19));
---
+++ jscr - domashni/jvScr - hw1/javaScript - hw 2/jvScrpthW2.js
@@ -0,0 +1,30 @@
+let myAge = 19.19.1997;
+let name = 14;
+let yearsSb = 19;
+let yearsUp = 26;
+// let num = Number(myAge);
+// let hghAge = ;  
+
+/*function hghAge(){
+    if(num > name){
+        return "true";          // Create 5th boolean variable that should be true if the first variable is higher then the second
+    }else {                     // variable. Ova e zadachata, ama ne se snaogjam da ja resham.
+        return "false";
+    }
+}
+sum1();       */
+
+function isGreaterThan(value1, value2) {            // Gotov kod od chatGPT. I ovoj mi dava eror
+    return value1 > value2;
+  }
+  
+  console.log(isGreaterThan(19.19.1997, 14));
+
+// -------------------------------------------------------------------------------------------------------------------------
+
+function dvd(var3, var4){      // 6th variable should show the reminder when you divide the third and the fourth
+  return var3 / var4;          //    variable. 
+}
+alert(dvd(26, 19)); 
+
+// Za prvata i posledna zadacha od ovaa domashna ne gi znam voopshto.
